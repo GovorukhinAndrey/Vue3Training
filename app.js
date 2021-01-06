2662c1d63603010604894d97008589df39bab08b
Andrey Govorukhin <andr.govorukhin@gmail.com>
2021-01-06 10:35:35 +0000

    Feat: Двусторонняя привязка через v-bind и v-on

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const App = {
         return {
             title: 'Список заметок',
             placeholder: 'Введите название заметки',
+            inputValue: '',
         }
-    }
+    },
+    methods: {
+        inputChangeHandler(event){
+            this.inputValue = event.target.value;
+        }
+    },
 }
 
 Vue.createApp(App).mount('#app')
