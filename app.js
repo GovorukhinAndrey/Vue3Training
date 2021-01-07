4d4ba3009d0f29b1d1cacbe9d51c2affa6a46170
Andrey Govorukhin <andr.govorukhin@gmail.com>
2021-01-07 09:51:01 +0000

    Feat: Пример работы с render()

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const h = Vue.h
+
 const app = Vue.createApp({
-    template: `
-        <div class="center card">
-            <h1>{{ title }}</h1>
-            <button class="btn" @click="title = 'Изменили!!'">Изменить</button>
-        </div>
-    `,
+    // template: `
+    //     <div class="center card">
+    //         <h1>{{ title }}</h1>
+    //         <button class="btn" @click="title = 'Изменили!!'">Изменить</button>
+    //     </div>
+    // `,
+    render() {
+        return h('div', {
+            class: 'card center'
+        }, [
+            h('h1', {}, this.title),
+            h('button', {class: 'btn', onClick: this.changeTitle}, 'Изменить')
+        ])
+    },
     data: () => ({
         title: 'Vue 3 Example Template'
-    })
+    }),
+    methods: {
+        changeTitle() {
+            this.title = 'Изменили!!'
+        }
+    },
 })
 
 app.mount('#app')
